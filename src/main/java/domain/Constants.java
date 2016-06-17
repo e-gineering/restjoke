4eea023fc963aae94028c4f02cf20334f0823158
Bryson Stevens <stevenbd@indiana.edu>
2016-06-17 11:26:18 -0400

    fixed URL to escape characters for javascript

--- src/main/java/domain/Constants.java
+++ src/main/java/domain/Constants.java
@@ -1,5 +1,5 @@
 package domain;
 
 public class Constants {
-	public static final String API_URL = "https://api.icndb.com/jokes/random?limitTo=[nerdy]";
+	public static final String API_URL = "https://api.icndb.com/jokes/random?limitTo=[nerdy]&escape=javascript";
 }
